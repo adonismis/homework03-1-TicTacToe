889b53d71a1f5f539c844e41a3fb2e26673ab346
adonis <adonis@adonistekiMacBook-Pro.local>
2014-07-27 10:32:54 +0800

    Add: responseJS #1

--- javascript/loader.js
+++ javascript/loader.js
@@ -1,11 +1,18 @@
+requirejs.config({
+	baseUrl: 'bower_components/',
+	paths:{
+		'jquery':'jquery/dist/jquery.min'
+	}
+});
+
 require(
-			[
-				'bower_components/jquery/dist/jquery.min.js',
-				'bower_components/jquery-ui/ui/jquery-ui.js',
-				'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
-				'javascript/main.js'
-			],function () {
-				console.log('Loader');
-				$('.grid').playRound();
-			}
+		[
+			'jquery',
+			'bower_components/jquery-ui/ui/jquery-ui.js',
+			'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
+			'javascript/main.js'
+		],function () {
+			console.log('Loader');
+			$('.grid').playRound();
+		}
 		);
